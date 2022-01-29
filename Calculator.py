e70d954a6cf655ca37973ab761442cbbf559432d
unknown <a_t_white@outlook.com>
2022-01-29 11:45:17 +0000

    adds function added

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,9 @@
 a = int(input("What would you like your first number to be?"))
 b = int(input("What would you like your second number to be?"))
 
-c = a + b
+def adds(x,y):
+    return x+y
+
+print(adds(a,b))
+
+
