e2a87a6d89b416c8443545ad24be7a972d0d41a4
unknown <a_t_white@outlook.com>
2022-01-31 16:35:13 +0000

    add z

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,8 @@
 a = int(input("What would you like your first number to be?"))
 b = int(input("What would you like your second number to be?"))
 
-def adds(x,y):
-    return x+y
+def adds(x,y, z=3):
+    return x+y+z
 
 print(adds(a,b))
 
